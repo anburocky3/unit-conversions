a93582b92c22af432f7c3f8057389f69eb2b9eeb
Anbu <15264938@users.noreply.github.com>
2021-01-18 03:26:55 +0530

    Added kg to lbs conversion

---
+++ src/Weight.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Anbu\UnitConversions;
+
+class Weight
+{
+     private float $kilograms;
+
+     public static function fromKilograms(float $kilograms): self
+     {
+         return new static($kilograms);
+     }
+
+     public function __construct(float $kilograms)
+     {
+         $this->kilograms = $kilograms;
+     }
+
+     public function toLbs(): float
+     {
+         return $this->kilograms * 2.2046;
+     }
+
+}
